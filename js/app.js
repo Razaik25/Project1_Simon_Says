b75da55c0b2637a6e6b55824d846031c00c28323
Razaik Boparai <razaikb@gmail.com>
2016-02-06 18:01:53 -0500

    Game is now functional for two players

--- js/app.js
+++ js/app.js
@@ -12,18 +12,21 @@ var $result = $('#result');
 // Declaring a object literal
 var simon_says = {
 
-    rounds: 1,       // no of rounds user plays
-    score: 0,        // current score of the user
-    genSeqArr: [],   //array to generate sequence lighting/sounds pattern
-    userSeqArr: [],  // array to keep track of user's pattern
-    player: 1,       // keep track if player 1 is playing or player 2
-    move: 0,          //current move of the user
-    start: false,     // to control when the displayPattern should be displayed
-    score: 0,         // keep the score of the user
-
+    rounds: 1,                      // no of rounds user plays
+    score: 0,                       // keep the score of the current user
+    genSeqArr: [],                  //array to generate sequence lighting/sounds pattern
+    userSeqArr: [],                 // array to keep track of user's pattern
+    playerTurn: '',                 // keep track if player 1 is playing or player 2
+    move: 0,                        //current move of the user
+    start: false,                   // to control when the displayPattern should be displayed  score: 0,                       //
+    player1Score: 0,                // store player 1 score
+    player2Score: 0,                // store player 2 score
+    player1Time: 0,                 // store player 1 time
+    player2TIme:0,                  // store player 2 time
+    playerOne: 'Player1',           // Player 1
+    playerTwo: 'Player2',           // Player 2
 
     startGame: function() {
-
       //Starts the game
       this.rounds = 1;
       this.addHandler();
@@ -33,7 +36,6 @@ var simon_says = {
     },
 
     newGame: function () {
-
       //called everytime a new game begins
       this.rounds = 1;
       this.newRound();
@@ -41,7 +43,6 @@ var simon_says = {
     },
 
     newRound: function () {
-
       // Resets the variables as new round begins
       this.genSeqArr =[];
       this.userSeqArr =[];
@@ -53,22 +54,33 @@ var simon_says = {
       }
       // reset the current user move to zero
       this.move = 0;
+
     },
 
     init: function () {
-
       // initialize the game
+      if (this.playerTurn === '') {
+        this.playerTurn = this.playerOne;
+      } else if (this.playerTurn === this.playerOne) {
+        this.playerTurn = this.playerTwo;
+      } else if (this.playerTurn === this.playerTwo) {
+        this.playerTurn = this.playerOne;
+      }
       this.showRound();
       this.showPlayer();
       this.userScore();
       this.displayTime();
       //$result.text('');
       $result.hide();
+      $('#whowon').hide();
+      $('#wholost').hide();
+      $('#winner').hide();
+      $('#loser').hide();
+      //$('#whowon').hide();
 
     },
 
     showRound: function () {
-
       // shows the current round number
      $('#round').fadeOut(500);
      $('#round').text('Round: '+ this.rounds);
@@ -77,39 +89,48 @@ var simon_says = {
     },
 
     showScore: function () {
-
-      // shows the current round number
+    // shows the current round number
      $('#score').fadeOut(500);
      $('#score').text('Score: '+ this.score);
      $('#score').fadeIn(500);
+     if (this.playerTurn === 'Player1'){
+       $('#score').text('Player1 Score: '+ this.score);
+     } else {
+       $('#score').text('Player2 Score: '+ this.score);
+     }
 
     },
 
     showPlayer: function () {
-
-      // shows the current player
-     $('#player').text('Player: '+ this.player);
+    // shows the current player
+     $('#player').text('Player: '+ this.playerTurn);
 
     },
 
     clearParameter: function () {
-      //clear player, round and timer
+    //clear player, round and timer
       $('#round').text('');
       $('#player').text('');
       $('#score').text('');
       stopTimer();
 
+      time = stopTimer();
+      // Updatedating player score
+      if (this.playerTurn === this.playerOne) {
+        this.player1Time = time;
+      } else if (this.playerTurn === this.playerTwo) {
+        this.player2TIme = time;
+      }
+
       function stopTimer() {
         clearTimeout(myTime);
         $('#timer').text('');
+        return myTime;
       }
-      $result.text('Game Ends! Nice Try!');
-      $result.show();
 
     },
 
     addHandler: function () {
-
       // add click evnts on start buttons and col no1 to col no4
       // capturing the current instance of this
       var that = this;
@@ -125,7 +146,6 @@ var simon_says = {
     },
 
     addHandlerCol: function () {
-
       // Add click event handlers for all the four columns
       var that = this;
       $col1.on('click', function () {
@@ -160,7 +180,6 @@ var simon_says = {
     },
 
     endGame: function () {
-
       //turn event hadlers off of all the columns off
       var that = this;
       /*$('.col').off();
@@ -193,11 +212,11 @@ var simon_says = {
 		       },500);
 
       that.clearParameter();
+      that.whoWon();
 
     },
 
     flash: function (column,count,time) {
-
       // lights up the clicked column
       // using animate() in jquery to implement the flashing for an interval of 2 seconds.
       var that = this;
@@ -227,7 +246,6 @@ var simon_says = {
     },
 
     displayPattern: function() {
-
     //random number between 1 and 4 as there are 4 columns.
     var that = this; // catching the current instance of this
      for (var i = 0; i < this.rounds; i++) {
@@ -279,7 +297,6 @@ var simon_says = {
     },
 
     displayTime: function () {
-
         // Display the seconds when the user clicks on start button
         counter = 0;
         myTime = setInterval(gettime, 1000);
@@ -291,7 +308,6 @@ var simon_says = {
     },
 
     userScore: function () {
-
       // Calculating the score
       if(this.rounds === 1) {
         this.score = 1;
@@ -299,16 +315,15 @@ var simon_says = {
         this.score = this.score * this.rounds;
       }
 
-      // // Displaying the score
-      // $('#score').fadeIn(500);
-      // $('#score').text('Score: '+ this.score);
-      // $('#score').fadeIn(500);
-
+      if (this.playerTurn === this.playerOne) {
+        this.player1Score = this.score;
+      } else if (this.playerTurn === this.playerTwo) {
+        this.player2Score = this.score;
+      }
 
     },
 
     countClicks: function (value) {
-
       //Gets the value of the column that was clicked and pushs it into userSeqArr Array
       this.userSeqArr.push(parseInt(value));
       // Call the match pattern function to compare the generated sequence with user clicks
@@ -317,25 +332,19 @@ var simon_says = {
     },
 
     matchPattern: function (value) {
-
       // function that checks if the elements in genSeqArr are same as userSeqArr
      var  that = this;
-     //if data attribute of value of the clicked column is same as the element in the genSeqArr
+     //To check if data attribute of value of the clicked column is same as the element in the genSeqArr
      //if it is not
      if(value !== this.genSeqArr[parseInt(that.move)]){
-
         that.endGame();
-
       }else{
-
         //increment the current user move
         this.move++;
-
       }
 
     // if all user has completed the whole  generated sequence
     if(this.move === this.genSeqArr.length){
-
       // increment the round
       this.rounds++;
       /*increment level, display it, flash the cols wait 1 second and then reset the game
@@ -350,16 +359,77 @@ var simon_says = {
 
   },
 
+  endturn: function () {
+
+
+  },
+
+  whoWon: function () {
+    // Determine who won out of two players
+    if(this.playerTurn === 'Player2'){
+
+      if(this.player1Score > this.player2Score){
+        //player one won
+        animateEnd();
+        $('#whowon').text('Player one wins- '+'Score: '+ this.player1Score);
+        $('#wholost').text('Player two Score: '+ this.player2Score);
+        $('#whowon').show();
+        $('#wholost').show();
+      } else if(this.player2Score > this.player1Score) {
+        //player two won
+        animateEnd();
+        $('#whowon').text('Player two wins- '+'Score: '+ this.player2Score);
+        $('#wholost').text('Player one Score: '+ this.player1Score);
+        $('#whowon').show();
+        $('#wholost').show();
+      } else { // if score is same check time
+        if(this.player1Time > this.player2TIme) {
+          // player  two won
+          animateEnd();
+          $('#whowon').text('Player two wins- '+'Score: '+ this.player2Score + ' Time: '+ this.player2TIme);
+          $('#wholost').text('Player one Score: '+ this.player1Score + ' Time: '+ this.player1Time);
+          $('#whowon').show();
+          $('#wholost').show();
+        } else {
+          // player one won
+          animateEnd();
+          $('#whowon').text('Player one wins- '+'Score: '+ this.player1Score + ' Time: '+ this.player1Time);
+          $('#wholost').text('Player two Score: '+ this.player2Score + ' Time: '+ this.player2Time);
+          $('#whowon').show();
+          $('#wholost').show();
+        }
+      }
+    } else {
+      // Have to change this to show which player turn and when to end
+      $result.text(this.playerTwo +'\'s Turn');
+      this.showScore();
+      $result.show();
+
+    }
+ }
+
 };
 
 $(document).ready(function(){
   console.log('script is linked');
   simon_says.startGame();
-});
 
+});
 
 // Function to diasble col event handlers for 10 seconds after the user matches wrong sequence
 // will see if i have to use this later, have to add the delay.
 function disablecols () {
   $('.col').css('pointer-events', 'none');
+}
+
+function animateEnd() {
+
+  $('#winner').animate({ left: "45%", top: "10%" }, 'slow').animate({
+                 left: "819px", top: "191px", opacity: 1 }, 500 );
+  $('#winner').show();
+
+  $('#loser').animate({ left: "30%", top: "20%" }, 'slow').animate({
+                 left: "822px", top: "306px", opacity: 1 }, 500 );
+  $('#loser').show();
+
 }
